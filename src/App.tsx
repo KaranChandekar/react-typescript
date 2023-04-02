deceb48b44bd733debc6d95a3ee327390985c2cc
Karan Chandekar <karanchandekar431@gmail.com>
2023-04-02 12:18:13 +0530

    Basic props

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,42 @@
 import "./App.css";
 import Greet from "./components/Greet";
+import Person from "./components/Person";
+import PersonList from "./components/PersonList";
 
 function App() {
+  const personName = {
+    first: "Steven",
+    last: "Strange",
+  };
+
+  const nameList = [
+    {
+      first: "John",
+      last: "Wick",
+    },
+    {
+      first: "Bruce",
+      last: "Wayne",
+    },
+    {
+      first: "Tony",
+      last: "Stark",
+    },
+    {
+      first: "Peter",
+      last: "Parker",
+    },
+    {
+      first: "Clark",
+      last: "Kent",
+    },
+  ];
+  
   return (
     <div className="App">
-      <Greet name="Karan" />
+      <Greet name="Karan" messageCount={10} isLoggedIn={true} />
+      <Person name={personName} />
+      <PersonList names={nameList} />
     </div>
   );
 }
---
+++ src/components/PersonList.tsx
@@ -0,0 +1,21 @@
+type personListProps = {
+  names: {
+    first: string;
+    last: string;
+  }[];
+};
+
+const PersonList = (props: personListProps) => {
+  return (
+    <div>
+      {props.names.map((name) => {
+        return (
+          <h2 key={name.first}>
+            {name.first} {name.last}
+          </h2>
+        );
+      })}
+    </div>
+  );
+};
+export default PersonList;
